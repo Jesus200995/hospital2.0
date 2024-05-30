4b516ec4f62b2bf8a845b7051ca18cde2c5bb553
Jesus200995 <jesus.riosgomez@utxicotepec.edu.com>
2024-05-30 15:26:58 -0600

    Diseño de Registro y Login

--- Frontend/sistema-hospital/src/router/index.js
+++ Frontend/sistema-hospital/src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RegisterUser from '@/components/registerUser.vue'
+import LoginView from '@/components/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser
     },
     {
       path: '/about',
